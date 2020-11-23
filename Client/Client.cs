d0c590308d9ff1a786965bd4179ccf67569c7d5c
jfeliz@camsoft.com.do <jfeliz@camsoft.com.do>
2020-11-23 03:25:22 -0500

    Add GetClients test

---
+++ Client/Client.cs
@@ -0,0 +1,23 @@
+﻿using JETech.NetCoreWeb;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace JETech.SIC.Core.UnitTest.Client
+{
+    public class Client
+    {
+        [Fact]
+        public async void GetClients() 
+        {
+            ActionQueryArgs<Clients.Models.ClientModel> args = new ActionQueryArgs<Clients.Models.ClientModel>();
+
+            JETech.SIC.Core.Clients.Domain.Client client = new Clients.Domain.Client();
+
+            var result = await client.GetClients(args);
+
+            Assert.NotNull(result.Data);
+        }
+    }
+}
